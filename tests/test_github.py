b2b4d78581cb716e6514a876033746ec8bfc4df2
Li Lin <guru.lin@gmail.com>
2023-09-11 17:56:43 +0800

    update github test case

--- tests/test_github.py
+++ tests/test_github.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 
 import pytest
 
@@ -6,8 +7,13 @@ from github_client.client import Client
 
 
 @pytest.mark.asyncio
-async def test_spring_boot():
+async def test_github_spring_boot():
     token = os.environ.get("GITHUB_TOKEN")
     client = Client(url="https://api.github.com/graphql", headers={"Authorization": f"Bearer {token}"})
     result = await client.pr_stats("spring-projects", "spring-boot")
-    print(result)
+    # simple example to show how to use the data returned from API call
+    pr1 = result.repository.pull_requests.nodes[0]
+    pr_create_time = datetime.strptime(pr1.created_at, "%Y-%m-%dT%H:%M:%SZ")
+
+    assert pr1
+    assert pr_create_time.year >= 2013
